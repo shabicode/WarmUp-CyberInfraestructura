9d7cf9c795fe0d82f67ce182ccbc190c9879b332
shabioks <monicaab.mtz95@gmail.com>
2026-03-08 19:04:42 -0600

    Feature: Se modifico el diseno y se agregaron comentarios, para el entregable.

--- WarmUp-CyberInfraestructura/Form1.Designer.cs
+++ WarmUp-CyberInfraestructura/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WarmUp_CyberInfraestructura
 {
-	partial class Form1
+	partial class frmPasswordValidator
 	{
 		/// <summary>
 		///  Required designer variable.
@@ -28,69 +28,118 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPasswordValidator));
 			txtPassword = new TextBox();
 			txtPasswordConfirm = new TextBox();
 			btnValidate = new Button();
 			label1 = new Label();
 			label2 = new Label();
+			panel1 = new Panel();
+			lblTitle = new Label();
+			panel1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// txtPassword
 			// 
-			txtPassword.Location = new Point(221, 150);
+			txtPassword.BackColor = Color.FromArgb(255, 249, 240);
+			txtPassword.BorderStyle = BorderStyle.FixedSingle;
+			txtPassword.Location = new Point(65, 95);
+			txtPassword.MaxLength = 32769;
 			txtPassword.Name = "txtPassword";
-			txtPassword.Size = new Size(248, 23);
+			txtPassword.Size = new Size(191, 23);
 			txtPassword.TabIndex = 0;
 			// 
 			// txtPasswordConfirm
 			// 
-			txtPasswordConfirm.Location = new Point(221, 215);
+			txtPasswordConfirm.BackColor = Color.FromArgb(255, 249, 240);
+			txtPasswordConfirm.BorderStyle = BorderStyle.FixedSingle;
+			txtPasswordConfirm.Location = new Point(65, 160);
 			txtPasswordConfirm.Name = "txtPasswordConfirm";
-			txtPasswordConfirm.Size = new Size(248, 23);
+			txtPasswordConfirm.Size = new Size(191, 23);
 			txtPasswordConfirm.TabIndex = 1;
 			// 
 			// btnValidate
 			// 
-			btnValidate.Font = new Font("Yu Gothic UI Semibold", 9F);
-			btnValidate.Location = new Point(274, 278);
+			btnValidate.AutoSize = true;
+			btnValidate.BackColor = Color.FromArgb(255, 179, 209);
+			btnValidate.BackgroundImage = (Image)resources.GetObject("btnValidate.BackgroundImage");
+			btnValidate.FlatAppearance.BorderSize = 0;
+			btnValidate.FlatStyle = FlatStyle.Popup;
+			btnValidate.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			btnValidate.ForeColor = Color.DarkSlateBlue;
+			btnValidate.Location = new Point(65, 200);
 			btnValidate.Name = "btnValidate";
-			btnValidate.Size = new Size(123, 31);
+			btnValidate.Size = new Size(191, 47);
 			btnValidate.TabIndex = 2;
-			btnValidate.Text = "Validate";
-			btnValidate.UseVisualStyleBackColor = true;
+			btnValidate.Text = " 🌸 Validate 🌸";
+			btnValidate.UseVisualStyleBackColor = false;
 			btnValidate.Click += btnValidate_Click;
 			// 
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Location = new Point(101, 158);
+			label1.BackColor = Color.Transparent;
+			label1.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold);
+			label1.ForeColor = Color.DarkSlateBlue;
+			label1.Location = new Point(65, 75);
 			label1.Name = "label1";
-			label1.Size = new Size(57, 15);
+			label1.Size = new Size(115, 17);
 			label1.TabIndex = 3;
-			label1.Text = "Password";
+			label1.Text = "Password 💜✨";
 			// 
 			// label2
 			// 
 			label2.AutoSize = true;
-			label2.Location = new Point(81, 218);
+			label2.BackColor = Color.Transparent;
+			label2.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold);
+			label2.ForeColor = Color.DarkSlateBlue;
+			label2.Location = new Point(65, 140);
 			label2.Name = "label2";
-			label2.Size = new Size(104, 15);
+			label2.Size = new Size(154, 17);
 			label2.TabIndex = 3;
-			label2.Text = "Confirm Password";
+			label2.Text = "Confirm Password ✨";
 			// 
-			// Form1
+			// panel1
+			// 
+			panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+			panel1.Controls.Add(lblTitle);
+			panel1.Location = new Point(0, 0);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(429, 32);
+			panel1.TabIndex = 4;
+			// 
+			// lblTitle
+			// 
+			lblTitle.AutoSize = true;
+			lblTitle.BackColor = Color.Transparent;
+			lblTitle.Font = new Font("Segoe UI Symbol", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			lblTitle.ForeColor = Color.Indigo;
+			lblTitle.Location = new Point(80, 9);
+			lblTitle.Name = "lblTitle";
+			lblTitle.Size = new Size(139, 17);
+			lblTitle.TabIndex = 5;
+			lblTitle.Text = "Password Validator";
+			// 
+			// frmPasswordValidator
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(648, 454);
+			BackColor = Color.LavenderBlush;
+			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+			BackgroundImageLayout = ImageLayout.None;
+			ClientSize = new Size(319, 322);
+			Controls.Add(panel1);
 			Controls.Add(label2);
 			Controls.Add(label1);
 			Controls.Add(btnValidate);
 			Controls.Add(txtPasswordConfirm);
 			Controls.Add(txtPassword);
-			Name = "Form1";
-			Text = "Form1";
-			Load += Form1_Load;
+			FormBorderStyle = FormBorderStyle.None;
+			Name = "frmPasswordValidator";
+			StartPosition = FormStartPosition.CenterScreen;
+			Text = "Password Validator";
+			panel1.ResumeLayout(false);
+			panel1.PerformLayout();
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -102,5 +151,7 @@
 		private Button btnValidate;
 		private Label label1;
 		private Label label2;
+		private Panel panel1;
+		private Label lblTitle;
 	}
 }
--- WarmUp-CyberInfraestructura/Form1.cs
+++ WarmUp-CyberInfraestructura/Form1.cs
@@ -3,37 +3,44 @@ using System.Windows.Forms;
 
 namespace WarmUp_CyberInfraestructura
 {
-	public partial class Form1 : Form
+	public partial class frmPasswordValidator : Form
 	{
-		private Regex PasswordRegex = new Regex(
-		@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).+$",
-		RegexOptions.Compiled
-		);
+		// Expresión regular que valida que la contraseña tenga:
+		// mayúscula, minúscula, número y símbolo
+		[GeneratedRegex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).+$", RegexOptions.Compiled)]
+		private static partial Regex PasswordValidationRegex();
 
-		public Form1()
+		// Creamos el objeto regex para usarlo en la validación
+		private Regex PasswordRegex = PasswordValidationRegex();
+
+		public frmPasswordValidator()
 		{
 			InitializeComponent();
 		}
 
+		// Funcion para validar la contraseña al hacer clic en el botón
 		private void btnValidate_Click(object sender, EventArgs e)
 		{
+			// Verifica que la contraseña cumpla con el formato requerido usando la expresión regular
 			if (!PasswordRegex.IsMatch(txtPassword.Text))
 			{
+				// Si no cumple, mostramos un mensaje de error y salimos
 				MessageBox.Show("Contrasena Invalida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
 
+			// Verificamos que ambas contraseñas sean iguales
 			if (txtPassword.Text != txtPasswordConfirm.Text)
 			{
+				// Si no coinciden, mostramos error y salimos
 				MessageBox.Show("Las contrasenas no coinciden", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
+
+			// Si pasó ambas validaciones, la contraseña es válida
 			MessageBox.Show("Contrasena Valida", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
 		}
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
 
-		}
 	}
 }
